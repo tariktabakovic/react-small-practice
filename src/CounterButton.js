92ba9df6f0a140066c37ab5cd382ce16afc05302
tariktabakovic <tariktab97@gmail.com>
2020-06-24 15:19:23 -0400

    create counterbutton functino with a button element inside

--- src/CounterButton.js
+++ src/CounterButton.js
@@ -1,30 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-export default class Counter extends React.Component {
-    constructor(props){
-        super(props);
-        this.state= {
-            number: 0
-        }
-    }
-    render(){
-        return (
-            <div>
-                <button onClick= {this._incrementNumber}>Add 1</button>
-                <div value={this.state.number}
-                     onChange={(event)=>{
-                     this._incrementNumber('number', event.target.value);
-                    }}>{this.state.number}
-                </div>
-            </div>
-        )
-    }
+function CounterButton(){
 
-    _incrementNumber = () =>{
-        this.setState({
-            number: this.state.number + 1
-        })
-    }
+    return (
+        <div>
+            <button></button>
+        </div>
+    )
 }
 
-
+export default CounterButton;
