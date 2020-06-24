f25a96f37192c63be75a606e64649c82cbb3f735
tariktabakovic <tariktab97@gmail.com>
2020-06-24 15:28:35 -0400

    creates helper function for incrementing up, and checks for color

--- src/Counter.js
+++ src/Counter.js
@@ -13,10 +13,19 @@ export default class Counter extends React.Component {
         return (
             <div>
                 {this.state.buttons.map((counter, i) =>(
-                    <CounterButton counter={counter.counter}/>
+                    <CounterButton key={i} counter={counter.counter} index={i} color={counter.color}/>
                 ))}
             </div>
         )
+    }
+
+    _increment = (index) => {
+        const newButtons = [...this.state.buttons];
+        newButtona[index].counter = this.state.buttons[index].counter + 1;
+        newButtons[index].color = newButtons[index].counter % 2 === 0? 'green' : 'red';
+        this.setState({
+            buttons: newButtons
+        })
     }
 }
 
