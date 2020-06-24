6b30b24d9bb5ef7e17f492fc297ede821c15a844
tariktabakovic <tariktab97@gmail.com>
2020-06-24 14:37:47 -0400

    imports Counter into App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Counter from './CounterButton';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Counter/>
   );
 }
 
