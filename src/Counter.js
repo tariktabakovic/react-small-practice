25b11594e26aba26091743b48fab94d1fccb2bb3
tariktabakovic <tariktab97@gmail.com>
2020-06-24 15:20:53 -0400

    renders CounterButton inside of CounterApp

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CounterButton from './CounterButton';
 
 export default class Counter extends React.Component {
     constructor(props){
@@ -11,7 +12,7 @@ export default class Counter extends React.Component {
     render(){
         return (
             <div>
-                
+                <CounterButton/>
             </div>
         )
     }
