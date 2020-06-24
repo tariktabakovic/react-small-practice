a2e310bf60303b3ceab0ac9d1070ce8930f0c1d0
tariktabakovic <tariktab97@gmail.com>
2020-06-24 15:17:02 -0400

    removes broken code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Counter from './CounterButton';
+import Counter from './Counter';
 import './App.css';
 
 function App() {
---
+++ src/Counter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default class Counter extends React.Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            buttons: []
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
